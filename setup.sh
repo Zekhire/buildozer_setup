f2a0d0195784198b185ae7554e30a49617bee89a
Krzysztof Wlodarczak <krzysztofprzem@gmail.com>
2021-05-15 22:50:21 +0200

    Added description and files

--- setup.sh
+++ setup.sh
@@ -1,13 +1,3 @@
-# buildozer_setup
-Repo that contains description how to make buildozer work. Works on 15.05.2021.
-
-## Requirements
-This repo was created and tested on Python 3.6.13.
-
-## Buildozer Setup
-First run following commands in order to install dependencies (it may contain large redundance, but who cares).:
-
-```bash
 sudo apt-get install openjdk-8-jdk -y
 sudo apt update
 sudo apt install -y git zip unzip openjdk-8-jdk python3-pip autoconf libtool pkg-config zlib1g-dev libncurses5-dev libncursesw5-dev libtinfo5 cmake libffi-dev libssl-dev
@@ -20,21 +10,17 @@ sudo apt-get install libncursesw5-dev libgdbm-dev libc6-dev -y
 sudo apt-get install zlib1g-dev libsqlite3-dev tk-dev -y
 sudo apt-get install libssl-dev openssl -y
 sudo apt-get install libffi-dev -y
-```
 
-Secondly run following commands in order to prepare virtual environment:
-
-```bash
 python3.6 -m virtualenv venv
 source ./venv/bin/activate
 pip install kivy
 pip install buildozer
 pip install cython
 pip install --upgrade Cython==0.29.19
-```
 
-Thirdly run following commands in order to download working sdkmanager and install API target 27 and build-tools with version > 25:
-```bash
+
+
+# https://linoxide.com/install-android-sdk-manager-linux-ubuntu-16-04/
 export PATH=/home/krzysztof/.buildozer/android/platform/android-sdk/tools:/home/krzysztof/.buildozer/android/platform/android-sdk/tools/bin:$PATH
 
 wget https://dl.google.com/android/repository/sdk-tools-linux-3859397.zip
@@ -43,11 +29,3 @@ mv tools home/krzysztof/.buildozer/android/platform/android-sdk/
 sdkmanager "platforms;android-27" "build-tools;24.0.2"
 buildozer android clean
 sdkmanager "platforms;android-27" "build-tools;25.0.2"
-```
-
-## Test
-Run following command:
-```bash
-buildozer android debug
-```
-
